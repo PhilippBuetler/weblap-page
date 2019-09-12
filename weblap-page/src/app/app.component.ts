7baa6e2b1737828ba7a5fd87c3f694e878b5196a
Philipp Bütler <philipp.buetler@trivelop.ch>
2019-09-12 11:05:57 +0200

    Page structured in different components.

--- weblap-page/src/app/app.component.ts
+++ weblap-page/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  menu_active: boolean = false;
   title = 'weblap-page';
+  constructor() { }
+  toggleMenu() {
+    return this.menu_active = !this.menu_active;
+  }
 }
