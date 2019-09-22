b831da4e61fc78b8bb5613c56ad25c2b0f9d3f11
Philipp Bütler <philipp.buetler@trivelop.ch>
2019-09-22 09:39:53 +0200

    Display articles from db

---
+++ weblap-page/src/app/frontpage/article.ts
@@ -0,0 +1,11 @@
+export class Article {
+  public title: string;
+  public subtitle: string;
+  public content: string;
+
+  constructor(title: string, subtitle: string, content: string) {
+    this.title = title;
+    this.subtitle = subtitle;
+    this.content = content;
+  }
+}
--- weblap-page/src/app/frontpage/frontpage.component.ts
+++ weblap-page/src/app/frontpage/frontpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from './article';
 
 @Component({
   selector: 'app-frontpage',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontpageComponent implements OnInit {
 
-  constructor() { }
+  backendApi: string = 'http://localhost:1234/frontpage';
 
-  ngOnInit() {
+  constructor() {
   }
 
+  public articles: Article[] = [];
+  title: string;
+  subtitle: string;
+  content: string;
+
+  ngOnInit() {
+    //this.getArticle().then(this.displayArticles);
+
+    this.getArticle().then((data) => {
+      console.log(data);
+      data.forEach((result) => {
+        var entry = result.newArticle;
+        this.articles.push(new Article(entry.title, entry.subtitle, entry.content));
+      });
+    });
+  }
+
+  async getArticle() {
+    let response = await fetch(this.backendApi);
+    console.log(response);
+    return await response.json();
+  }
+
+  displayArticles(results)  {
+    console.log(results);
+    results.forEach((result) =>{
+      console.log(result.newArticle.title);
+      this.title = result.newArticle.title;
+   });
+  }
 }
