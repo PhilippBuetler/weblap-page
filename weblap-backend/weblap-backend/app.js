c317ec2ee533175a024e13bb5661a93b67866e89
Philipp Bütler <philipp.buetler@trivelop.ch>
2019-10-01 10:59:20 +0200

    Backend: implement jwt tokens

--- weblap-backend/weblap-backend/app.js
+++ weblap-backend/weblap-backend/app.js
@@ -4,6 +4,7 @@ var multer = require('multer');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const errorHandler = require('./_helpers/error-handler');
 
 var DIR = 'uploads/';
 
@@ -60,40 +61,12 @@ app.use((req, res, next) => {
 
 app.use('/', indexRouter);
 app.use('/frontpage', frontpageRouter);
+app.use('/users', require('./users/users.controller'));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/', express.static(path.join(__dirname, 'angular')));
 
-// catch 404 and forward to error handler
-//app.use(function (req, res, next) {
-//    var err = new Error('Not Found');
-//    err.status = 404;
-//    next(err);
-//});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
- //production error handler
- //no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+app.use(errorHandler);
 
 app.post('/frontpage', (req, res) => {
     console.log(req.body);
